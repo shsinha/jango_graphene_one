3ad9df6b62024b72079be012c8334fdd5de368c1
shsinha <shantanusinha1@gmail.com>
2019-11-07 13:56:40 -0600

    done

---
+++ cookbook/cookbook/ingredients/models.py
@@ -0,0 +1,21 @@
+# cookbook/ingredients/models.py
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name='ingredients', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
